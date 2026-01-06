270bd7125f3403c00b6898ceac83ca7faaa5b364
amari-trotman <113646732+amari-trotman@users.noreply.github.com>
2026-01-06 16:40:34 -0500

    Refactor isAppropiateForWeather function to include style validation

--- weather.py
+++ weather.py
@@ -1,2 +1,5 @@
 def isAppropiateForWeather(style, temperatureF):
     warmthLevels = {'casual': (50, 95), 'business-casual': (55, 90), 'semi-formal': (60, 85), 'business-formal': (60, 80)}
+
+    if style not in warmthLevels:
+        return True # will not judge unknown styles
