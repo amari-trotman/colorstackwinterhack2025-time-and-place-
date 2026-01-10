ba66bc39411f3f71c3d679232ba95834eac87b4e
amari-trotman <113646732+amari-trotman@users.noreply.github.com>
2026-01-09 20:02:43 -0500

    Initialize webcam.js with element references for video, canvas, result, and details

---
+++ static/js/webcam.js
@@ -0,0 +1,5 @@
+const video = document.getElementById('video');
+const canvas = document.getElementById('canvas');
+const result = document.getElementById('result');
+const details = document.getElementById('details');
+
