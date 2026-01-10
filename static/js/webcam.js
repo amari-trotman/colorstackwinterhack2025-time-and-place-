69f7641ec34653c59d240ecd6843de10dca5ba18
amari-trotman <113646732+amari-trotman@users.noreply.github.com>
2026-01-09 20:05:15 -0500

    Add functionality to access webcam and stream video to the video element

--- static/js/webcam.js
+++ static/js/webcam.js
@@ -3,3 +3,5 @@ const canvas = document.getElementById('canvas');
 const result = document.getElementById('result');
 const details = document.getElementById('details');
 
+navigator.mediaDevices.getUserMedia({ video: true })
+    .then(stream => video.srcObject = stream);
